bfc8352ec45444b2f5971312e6023136e579b5ad
Masayuki Onishi <omccello@gmail.com>
2020-12-06 12:42:25 +0900

    docker環境追加

---
+++ deploy_dev.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -eux
+source "$(dirname $0)/bin/conf"
+
+### CREATE DIRECTORIES ###
+mkdir -p "$logdir" "$datadir" "$datadir/counters"
+chown www-data:www-data "$logdir" "$datadir" "$datadir/counters"
+
+### INSTALL THIS SYSTEM ###
+rsync -av --delete "$(dirname $0)/bin/" "$appdir/"
+chown www-data:www-data "$appdir" -R
+
+### EDIT FETCH CGI ###
+cd "$appdir"
+rnd=$(cat /dev/urandom | tr -cd 0-9a-zA-Z | head -c 32)
+[ -e "/home/onishi/rnd" ] && rnd=$(cat /home/onishi/rnd )
+mv "fetch" "fetch_$rnd.cgi"
+
+### PULL DATA ###
+# rm -rf "${contentsdir:?}"
+cd "$wwwdir"
+# git clone "https://github.com/$contents_owner/$contents"
+chown www-data:www-data "$contentsdir" -R
+
+### RESTORE DATADIR ###
+rm -rf "$datadir/posts"
+rm -rf "$datadir/pages"
+
+### INITIALIZE ###
+touch "$datadir/INIT"
+chown www-data:www-data "$datadir/INIT"
+$appdir/fetch_$rnd.cgi
+
+echo "call fetch_$rnd.cgi from GitHub"
---
+++ docker/ubuntu/apache/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# service apache2 restart
+
+/root/bash_cms_server/deploy_dev.sh
+
+/bin/bash
